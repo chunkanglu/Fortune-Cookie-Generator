ac6e0d038e7dcef9f1b22c9af592c63fb661cc8e
Chun Kang Lu <chunkanglu@gmail.com>
2022-05-21 11:06:36 -0400

    added cookie EST midnight expiry

--- gen_cookie.py
+++ gen_cookie.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, make_response, request
+from datetime import datetime, date, time, timedelta
 app = Flask(__name__)
 
 from random import randint
@@ -9,12 +10,17 @@ def index():
     if (request.cookies.get("fortune") == None):
         fortune = openCookie()
         resp = make_response(render_template("index.html", fortune=fortune))
-        resp.set_cookie("fortune", fortune)
+        resp.set_cookie("fortune", fortune, expires=get_midnight())
         return resp
     else:
         return render_template("index.html", fortune=request.cookies.get("fortune"))
 
-@app.route('/fortune')
+# @app.route('/fortune')
+
+def get_midnight():
+    today_date = date.today()
+    return datetime.combine(today_date + timedelta(days=1), time()) + timedelta(hours=4) # time() defaults to midnight UTC, +4 for EST
+
 def openCookie() -> str:
     with open("fortunes.txt", "r") as fortunes:
         arr = fortunes.readlines()
@@ -22,5 +28,3 @@ def openCookie() -> str:
 
 if __name__ == '__main__':
   app.run(debug=True)
-
-print(openCookie())
